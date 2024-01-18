e97b7603acac161b649b1bf07947636a22e1eca0
Juan Diego Castaño <juan.castano@juandica-m-rj6l.kennasec.com>
2024-01-18 16:08:45 -0500

    home page and connector page

---
+++ apps/dummy-app/app/Connectors/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default async function Index() {
+  return (
+    <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+            <h1>
+              Connectors page
+            </h1>
+          </div>
+        </div>
+      </div>
+  );
+}
--- apps/dummy-app/app/page.tsx
+++ apps/dummy-app/app/page.tsx
@@ -20,8 +20,14 @@ export default async function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome dummy-app
+              Home page
             </h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum tincidunt dapibus. Duis in enim non tortor lacinia venenatis. Morbi luctus nibh id orci porttitor, a luctus ex convallis. Donec quis porta mauris. Mauris feugiat lobortis venenatis. Mauris lacinia urna ut pellentesque suscipit. Proin ut diam vitae enim tincidunt facilisis. Praesent ultricies ante diam. In ex ante, tincidunt sed aliquet iaculis, dictum nec leo. Proin tempus vestibulum est ac mollis. Cras venenatis, mauris vitae maximus sollicitudin, tortor dolor cursus nisl, et faucibus orci augue a neque. In commodo eleifend nibh id tempus. Nullam massa odio, volutpat eget porttitor vitae, feugiat vitae nisl.
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum tincidunt dapibus. Duis in enim non tortor lacinia venenatis. Morbi luctus nibh id orci porttitor, a luctus ex convallis. Donec quis porta mauris. Mauris feugiat lobortis venenatis. Mauris lacinia urna ut pellentesque suscipit. Proin ut diam vitae enim tincidunt facilisis. Praesent ultricies ante diam. In ex ante, tincidunt sed aliquet iaculis, dictum nec leo. Proin tempus vestibulum est ac mollis. Cras venenatis, mauris vitae maximus sollicitudin, tortor dolor cursus nisl, et faucibus orci augue a neque. In commodo eleifend nibh id tempus. Nullam massa odio, volutpat eget porttitor vitae, feugiat vitae nisl.
+            </p>
           </div>
         </div>
       </div>
