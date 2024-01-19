24336bf0536cbb222dc1053a00212362ef6d0387
Juan Diego Castaño <juan.castano@juandica-m-rj6l.kennasec.com>
2024-01-19 11:27:37 -0500

    Space to show normal components and magnetic components

--- apps/dummy-app/app/page.tsx
+++ apps/dummy-app/app/page.tsx
@@ -20,14 +20,20 @@ export default async function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Home page
+              Dummy App
             </h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum tincidunt dapibus. Duis in enim non tortor lacinia venenatis. Morbi luctus nibh id orci porttitor, a luctus ex convallis. Donec quis porta mauris. Mauris feugiat lobortis venenatis. Mauris lacinia urna ut pellentesque suscipit. Proin ut diam vitae enim tincidunt facilisis. Praesent ultricies ante diam. In ex ante, tincidunt sed aliquet iaculis, dictum nec leo. Proin tempus vestibulum est ac mollis. Cras venenatis, mauris vitae maximus sollicitudin, tortor dolor cursus nisl, et faucibus orci augue a neque. In commodo eleifend nibh id tempus. Nullam massa odio, volutpat eget porttitor vitae, feugiat vitae nisl.
-            </p>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc interdum tincidunt dapibus. Duis in enim non tortor lacinia venenatis. Morbi luctus nibh id orci porttitor, a luctus ex convallis. Donec quis porta mauris. Mauris feugiat lobortis venenatis. Mauris lacinia urna ut pellentesque suscipit. Proin ut diam vitae enim tincidunt facilisis. Praesent ultricies ante diam. In ex ante, tincidunt sed aliquet iaculis, dictum nec leo. Proin tempus vestibulum est ac mollis. Cras venenatis, mauris vitae maximus sollicitudin, tortor dolor cursus nisl, et faucibus orci augue a neque. In commodo eleifend nibh id tempus. Nullam massa odio, volutpat eget porttitor vitae, feugiat vitae nisl.
-            </p>
+          </div>
+          <div id="middle-content">
+            <div className="container-cmp rounded shadow">
+              <h1>
+                Normal components
+              </h1>
+            </div>
+            <div className="container-cmp rounded shadow">
+              <h1>
+                Magnetic components
+              </h1>
+            </div>
           </div>
         </div>
       </div>
